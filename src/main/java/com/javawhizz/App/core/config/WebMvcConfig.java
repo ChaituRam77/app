c2157fc27396cffffc16e1738b34df2f20967c67
chaituRam77 <chaitu4tech@gmail.com>
2023-03-26 23:51:21 +0200

    Enabled CORS

---
+++ src/main/java/com/javawhizz/App/core/config/WebMvcConfig.java
@@ -0,0 +1,23 @@
+package com.javawhizz.App.core.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    public void addCorsMapping(CorsRegistry registry){
+        registry
+                .addMapping("/*")
+                .allowedOrigins("*")
+                // .allowedOriginPatterns("")
+                .allowCredentials(false)
+                .allowedHeaders("*")
+                .exposedHeaders("*")
+                .maxAge(60 *30)
+                .allowedMethods("*")
+        ;
+    }
+
+}
